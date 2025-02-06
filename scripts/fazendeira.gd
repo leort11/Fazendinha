a14619c9b78a383c2d188da6a1dcac566245bd03
leort11 <leonardo.figueiredo31@hotmail.com>
2025-02-06 11:31:07 -0300

    adicionado: HUD das sementes, e azaleias animadas.

--- scripts/fazendeira.gd
+++ scripts/fazendeira.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 70.0
 const JUMP_VELOCITY = -400.0
 
@@ -9,7 +8,6 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		pass
 		#velocity.y += gravity * delta
--- scripts/tomate.gd
+++ scripts/tomate.gd
@@ -1,17 +1,27 @@
 extends Area2D
-
+var sementes = 1
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready():	
 	hide()
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$"../HUD/semente_tomate".text = str("Sementes de tomate = ", sementes)
 
 
 func _on_body_entered(body):
-	show()
-	$AnimatedSprite2D.frame = 0
+	if sementes >= 1:
+		sementes -= 1
+		show()
+		$AnimatedSprite2D.frame = 0
+		await get_tree().create_timer(3.0).timeout
+		$AnimatedSprite2D.frame = 1
+		await get_tree().create_timer(5.0).timeout
+		$AnimatedSprite2D.frame = 2
+		await get_tree().create_timer(7.0).timeout
+		$AnimatedSprite2D.frame = 3
+		await get_tree().create_timer(10.0).timeout
+		$AnimatedSprite2D.frame = 4
